8221d738ca2c5b8153c6611739ef0cbccf1e8ad4
patsicko <patsicko@g>
2022-08-30 01:34:09 +0200

    css

---
+++ src/componemts/testimonials/testmonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./testmonials.module.css"
+
+const Testmonials = () => {
+  return (
+    <div>Testmonials</div>
+  )
+}
+
+export default Testmonials
